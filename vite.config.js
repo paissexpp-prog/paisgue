57e39dfbfbf46cc8652c02c32bcb7f59430837b3
root <root@ruangotp.servarica.net>
2026-03-15 05:25:52 +0000

    ppd

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import vitePrerender from 'vite-plugin-prerender'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-// Mengatasi masalah __dirname pada Node dengan "type": "module"
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins:,
-    })
-  ],
+  plugins: [react()],
 })
