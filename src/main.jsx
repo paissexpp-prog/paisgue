3417d09184bf89ab90c8e25aeb06aae94645e40d
paissexpp-prog <paissexpp@gmail.com>
2026-06-16 05:45:52 +0000

    asek

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,28 @@ import './index.css'
 import { ThemeProvider } from './context/ThemeContext'
 import { HelmetProvider } from 'react-helmet-async'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </HelmetProvider>
-  </React.StrictMode>,
-)
+const root = document.getElementById('root')
 
+// react-snap butuh hydrate bukan render saat halaman sudah pre-rendered
+if (root.hasChildNodes()) {
+  ReactDOM.hydrateRoot(
+    root,
+    <React.StrictMode>
+      <HelmetProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </HelmetProvider>
+    </React.StrictMode>
+  )
+} else {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <HelmetProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </HelmetProvider>
+    </React.StrictMode>
+  )
+}
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import prerender from 'vite-plugin-prerender'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  plugins: [
-    react(),
-    prerender({
-      staticDir: path.join(__dirname, 'dist'),
-      routes: ['/'],
-      renderer: new prerender.PuppeteerRenderer({
-        renderAfterTime: 3000,
-        headless: true,
-      }),
-    }),
-  ],
+  plugins: [react()],
 })
